b19e6279518ba82360f029af5866f1da4969bff6
Altamiro B. Cardoso de Sousa <87079817+altamirosousa@users.noreply.github.com>
2021-12-09 23:53:38 -0300

    exemplos executados durante a aula

---
+++ PycharmProjects/CursoEmVideo/aula08.py
@@ -0,0 +1,8 @@
+#import bebida              --importa todas as funções
+#from doce import pudim     --importa apenas uma função
+import math
+importe emoji
+
+num = int(input('Digite um numero: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igua {}'.format(num, raiz))
---
+++ PycharmProjects/CursoEmVideo/aula08_desafio016.py
@@ -0,0 +1,4 @@
+#C1PQ leia um numero Real qualquer pelo teclado e mostre na tela a sua porção inteira
+
+
+
